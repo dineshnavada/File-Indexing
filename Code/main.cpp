f491dc13f4b35217c2099ee453047cf63918a187
dineshnavada <dineshkrnavada@gmail.com>
2018-03-30 22:19:18 +0530

    Added Search functionality
    New class indexList is added

--- Code/main.cpp
+++ Code/main.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <cstdlib>
 #include<iomanip>
+
 using namespace std;
 
 class student
@@ -15,7 +16,7 @@ class student
     friend class LinkedList;
     friend fstream& operator<<(fstream& out, student s)
     {
-        out<<setw(25)<<s.addr<<setw(15)<<s.usn;
+        out<<s.usn<<setw(25)<<s.addr;
         return out;
     }
 
@@ -27,9 +28,9 @@ void student::getdata(student s[], int icount)
     fi.open("student.txt",ios::in|ios::out);
     while(i<icount)
     {
-        addr=fi.tellg();
+        s[i].addr=fi.tellp();
         fi>>s[i].usn>>s[i].name;
-        cout<<i<<" "<<s[i].usn<<s[i].name<<endl;
+        //cout<<i<<" "<<s[i].usn<<s[i].name<<endl;
         i++;
     }
     cout<<"\n\n";
@@ -46,22 +47,64 @@ void LinkedList::insert_list(student s[], int icount)
     list<student>::iterator it;
 
     for(int i=0;i<icount;i++) student_list.push_back(s[i]);
-    cout<<"size:"<<student_list.size();
+    //cout<<"size:"<<student_list.size();
 }
 void LinkedList::create_index()
 {
     fstream indexFile;int indexCount=1;
     indexFile.open("index.txt",ios::out);
     list<student>::iterator it;
-    indexFile<<setw(15)<<"Index"<<setw(25)<<"Relative Byte Address"<<setw(15)<<"USN"<<"\n";
+    //indexFile<<setw(15)<<"Index"<<setw(25)<<"Relative Byte Address"<<setw(15)<<"USN"<<"\n";
     for(it=student_list.begin();it!=student_list.end();it++){
-         indexFile<<setw(12)<<indexCount<<"***";
          indexFile<< *it;
+        // indexFile<<setw(12)<<indexCount;
          indexFile<<endl;
          indexCount++;
          }
     indexFile.close();
 }
+class indexList
+{
+    private:string usn;
+            long int raddr;
+    public:
+            indexList(string temp)
+            {
+                usn=temp;
+            }
+            void searchIndex(indexList ind, int indexCount);
+
+};
+void indexList::searchIndex(indexList ind, int indexCount)
+            {
+                indexList temp("\0");
+                fstream indexFile,studentFile;
+                int flag=0;
+                indexFile.open("index.txt",ios::in);
+                while((indexCount)>0)
+                {
+                    indexFile>>temp.usn>>temp.raddr;
+                    indexCount--;
+                    //cout<<temp.usn<<"  "<<ind.usn<<"\n";
+                    if(ind.usn==temp.usn)
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag)
+                {
+                    studentFile.open("student.txt", ios::in);
+                    string studentStr;
+                    studentFile.seekg(temp.raddr+1,ios::beg);
+                    getline(studentFile, studentStr);
+                    cout<<"\nRecord is \n"<<studentStr;
+                }
+                else
+                    cout<<"\nRecord not found!";
+                indexFile.close();
+                studentFile.close();
+            }
 int main()
 {
     int icount=-1;
@@ -73,12 +116,16 @@ int main()
         fi>>i>>j;
         icount++;
     }
-    cout<<icount<<endl;
+    //cout<<icount<<endl;
     fi.close();
     student s[icount];
     s[0].getdata(s,icount);
     LinkedList ll;
     ll.insert_list(s,icount);
     ll.create_index();
+    cout<<"\nEnter the USN:";
+    cin>>i;
+    indexList indexed(i);
+    indexed.searchIndex(indexed, icount);
     return 0;
 }
